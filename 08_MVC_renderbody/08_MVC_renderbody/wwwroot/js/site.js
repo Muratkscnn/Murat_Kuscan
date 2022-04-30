f9f1961eecb762985cbf838e299eb4b0f49c2e76
Muratkscnn <98876141+Muratkscnn@users.noreply.github.com>
2022-05-01 02:26:50 +0300

    1.05.22

--- 08_MVC_renderbody/08_MVC_renderbody/wwwroot/js/site.js
+++
@@ -1,9 +0,0 @@
-﻿$(document).ready(function () {
-    $("li a").each(function () {
-        if ($(this).attr("href") == window.location.pathname)
-            
-            $(this).addClass("active");
-    })
-})
-
-
