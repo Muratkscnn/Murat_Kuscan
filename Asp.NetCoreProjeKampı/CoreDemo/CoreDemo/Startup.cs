91d14eefc7e27b20b31ea4728a3ab60fadf59fd4
Muratkscnn <98876141+Muratkscnn@users.noreply.github.com>
2022-05-05 16:48:20 +0300

    5.05.22

--- Asp.NetCoreProjeKampı/CoreDemo/CoreDemo/Startup.cs
+++ Asp.NetCoreProjeKampı/CoreDemo/CoreDemo/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -31,6 +32,10 @@ namespace CoreDemo
                 var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
                 config.Filters.Add(new AuthorizeFilter(policy));
             });
+            services.AddMvc();
+            services.AddAuthentication(
+                CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(x => { x.LoginPath = "/Login/Index"; });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,8 +55,8 @@ namespace CoreDemo
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
