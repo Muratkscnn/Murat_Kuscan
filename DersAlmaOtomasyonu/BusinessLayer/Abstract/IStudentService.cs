f8b44ec54eee00731c9ac9e6a6cb0e9355d3f136
Muratkscnn <98876141+Muratkscnn@users.noreply.github.com>
2022-05-05 13:52:54 +0300

    5.05.22

---
+++ DersAlmaOtomasyonu/BusinessLayer/Abstract/IStudentService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IStudentService
+    {
+        void StudentAdd(Student student);
+        void StudentDelete(Student student);
+        void StudentUpdate(Student student);
+        List<Student> GetList();
+        Student GetById(int id);
+    }
+}
