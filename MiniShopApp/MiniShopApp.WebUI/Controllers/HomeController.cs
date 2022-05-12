3ab572765e2ab19702cfcc72a05740cf59cc9b8e
Muratkscnn <98876141+Muratkscnn@users.noreply.github.com>
2022-05-12 18:28:15 +0300

    12.05.22

--- MiniShopApp/MiniShopApp.WebUI/Controllers/HomeController.cs
+++ MiniShopApp/MiniShopApp.WebUI/Controllers/HomeController.cs
@@ -1,15 +1,15 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CoreDemo.Controllers
+namespace MiniShopApp.WebUI.Controllers
 {
-    public class DashboardController : Controller
+    public class HomeController : Controller
     {
-        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
